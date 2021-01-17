1f711b5731ef7f17a6c8f239429ea66766b09320
עדן גואטה <eden@h-MacBook-Pro-sl-dn.local>
2021-01-17 02:12:22 +0200

    HW's done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-class App extends React.Component {
+function App() {
+  const [albums, setAlbums] = useState([]);
+  const [photos, setPhotos] = useState([]);
 
-  render() {
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/albums')
+      .then(res => res.json())
+      .then(a => setAlbums(a))
+  },[]);
+
+  function getPhotos(albumId){
+    fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`)
+      .then(res => res.json())
+      .then(p => setPhotos(p))
+  }
+
     return (
         <div className="App">
           <h1>Select an album:</h1>
-
+          <select onChange={(e)=>getPhotos(e.target.value)}>
+            <option>Select...</option>
+            {albums.map(album=>{
+              return <option key={album.id} value={album.id}>{album.title}</option>
+            })}
+          </select>
+          <div className="gallery">
+            {photos.map((photo)=>{
+              return <img key={photo.id} src={photo.thumbnailUrl} alt={photo.title}/>
+            })}
+          </div>
         </div>
     );
-  }
-
 }
 
 export default App;
